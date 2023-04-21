ac54950b69b036583a920d483fd0a238b78923d9
Diego Ferreira <diegoferreira777@hotmail.com>
2023-04-21 17:10:52 -0300

    advances in layout

--- src/components/NewHabitForm/index.tsx
+++ src/components/NewHabitForm/index.tsx
@@ -1,3 +1,4 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 
 export const NewHabitForm = () => {
@@ -18,6 +19,20 @@ export const NewHabitForm = () => {
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         What is the recurrence?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        <Checkbox.Root className="flex items-center gap-3 group">
+          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+          </div>
+
+          <span className="text-white leading-tight">
+            Monday
+          </span>
+        </Checkbox.Root>
+      </div>
 
       <button
         type="submit"
